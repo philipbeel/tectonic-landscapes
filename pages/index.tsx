4032e507b042ad3be6eb4937b5469376a1657914
philipbeel <philipbeel@googlemail.com>
2020-06-17 22:34:42 +0100

    Add: github toolbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,21 @@ import Head from "next/head";
 
 import { getGithubPreviewProps, parseJson } from "next-tinacms-github";
 import { GetStaticProps } from "next";
+import { usePlugin } from "tinacms";
+import {
+  useGithubJsonForm,
+  useGithubToolbarPlugins,
+} from "react-tinacms-github";
 
 export default function Home({ file }) {
-  const data = file.data;
+  const formOptions = {
+    label: "Home Page",
+    fields: [{ name: "title", component: "text" }],
+  };
+  const [data, form] = useGithubJsonForm(file, formOptions);
+  usePlugin(form);
+  useGithubToolbarPlugins();
+
   return (
     <div className="container">
       <Head>
